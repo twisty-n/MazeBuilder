b578cf9321e71c5b90e30c60c56283d6e11cd2f7
Tristan Newmann <tristan_dev@tristans-mbp-3.newcastle.edu.au>
2015-01-12 13:40:16 +1100

    Added documentation to the mazeCanvas class

--- MazeCanvas.py
+++ MazeCanvas.py
@@ -5,14 +5,14 @@ MazeBuilder, Stroke Recovery Project, HMRI
 Author: Tristan Newmann
 Developed with 2.7
 
-File: Debug.py
+File: MazeCanvas.py
+
 
 Module contains the implementation
 """
 
 # Imports
 import Debug
-import math
 from Tkinter import Canvas, Frame, BOTH, Menu
 from DiaDoges import NodeDialog
 
@@ -73,9 +73,13 @@ class MazePlannerCanvas(Frame):
                 "event" : None
             }
         self._status = status
+        self._edges = {}
         self._construct(parent)
 
     def _construct(self, parent):
+        """
+        Construct all of the event bindings and callbacks for mouse events
+        """
         self._canvas.focus_set()
         self._canvas.bind("<B1-Motion>", lambda event, m_event=Event.DRAG_M1: self._handle_mouse_events(m_event, event))
         self._canvas.bind("<B2-Motion>", lambda event, m_event=Event.DRAG_M2: self._handle_mouse_events(m_event, event))
@@ -89,11 +93,29 @@ class MazePlannerCanvas(Frame):
         self._canvas.bind("<Enter>", lambda event: self._canvas.focus_set())
 
     def _handle_mot(self, m_event, event):
+        """
+        Callback function to handle movement of the mouse
+
+        Function updates the mouse location status bar as well as setting cache values to the current location
+        of the mouse
+
+        :m_event:           The specifier for the type of event that has been generated
+        :event:             The tk provided event object
+        """
         self._status.set_text("Mouse X:" + str(event.x) + "\tMouse Y:" + str(event.y))
         self._cache["x"] = event.x
         self._cache["y"] = event.y
 
     def _handle_mouse_events(self, m_event, event):
+        """
+        Function that routes mouse events to the appropriate handlers
+
+        Prints logging and UI information about the state of the mouse and then routes
+        the mouse event to the appropriate handler
+
+        :m_event:           The specifier for the tupe of event that has been generated
+        :event:             The tk provided event object
+        """
         self._status.set_text("Mouse X:" + str(self._cache["x"]) + "\tMouse Y:" + str(self._cache["y"]))
         Debug.printet(event, m_event, Debug.Level.INFO)
         self._cache["event"] = event
@@ -102,11 +124,28 @@ class MazePlannerCanvas(Frame):
 
 
     def _begin_node_drag(self, coords):
+        """
+        Handles starting operations for dragging a node
+
+        Updates the cache information regarding a node drag event
+
+        :coords:            The mouse coordinates associated with this event
+        """
         # Determine which node has been selected, cache this information
         item = self._get_current_item(coords)
         self._update_cache(item, coords)
 
     def _end_node_drag(self, coords):
+        """
+        Performs actions to complete a node drag operation
+
+        Validates node location, and other associated object information and updates the cache
+        when a node drag is completed
+
+        :coords:            The coordinates associated with this event
+        """
+
+        # TODO add in local object manager information
         if self._command_cache is Event.D_CLICK_M1 or None:
             # Don't dispatch if its the result of a double click
             return
@@ -131,6 +170,11 @@ class MazePlannerCanvas(Frame):
         # TODO Post the information to the node manager
 
     def _execute_drag(self, coords):
+        """
+        Updates object position on canvas when user is dragging a node
+
+        :param coords:          The coordinates associated with this event
+        """
         # Update the drawing information
         delta_x = coords[0] - self._cache["x"]
         delta_y = coords[1] - self._cache["y"]
@@ -141,6 +185,13 @@ class MazePlannerCanvas(Frame):
         self._cache["y"] = coords[1]
 
     def _launch_menu(self, coords):
+        """
+        Callback function in response to the pressing of the Return key
+
+        Launches a context menu based on the location of the mouse
+        :param coords:
+        :return:
+        """
         # Launch a context menu based on the coords of the mouse
         item = self._get_current_item((self._cache["x"], self._cache["y"]))
         p_menu = Menu(self._canvas)
@@ -157,9 +208,15 @@ class MazePlannerCanvas(Frame):
             p_menu.add_command(label="Mark as start", command=lambda: Debug.printi("New starting node", Debug.Level.INFO))
 
         updated_coords = self._canvas_to_screen((self._cache["x"], self._cache["y"]))
-        p_menu.post(updated_coords[0], updated_coords[1])
+        p_menu.tk_popup(updated_coords[0], updated_coords[1])
 
     def _canvas_to_screen(self, coords):
+        """
+        Convert canvas coordinates into screen coordinates
+
+        :param coords:              The current canvas coordinates
+        :return:
+        """
         # upper left corner of the visible region
         x0 = self._canvas.winfo_rootx()
         y0 = self._canvas.winfo_rooty()
@@ -170,12 +227,22 @@ class MazePlannerCanvas(Frame):
         return (int(wx0), int(wy0))
 
     def _begin_edge(self, coords):
+        """
+        Begin recording information regarding the placement of an edge
+
+        :param coords:               The coordinates associated with this event
+        """
         # Record the starting node
         self._edge_cache["item_start"] = self._get_current_item((self._cache["x"], self._cache["y"]))
         self._edge_cache["x_start"] = self._cache["x"]
         self._edge_cache["y_start"] = self._cache["y"]
 
     def _end_edge(self, coords):
+        """
+        Perform the operations required to complete an edge creation operation
+        :param coords:
+        :return:
+        """
         # Record the ending node\
         # Check if the cursor is over a node,
         # if it is, perform normally
@@ -183,10 +250,17 @@ class MazePlannerCanvas(Frame):
         # Post the edge information to the object manager
         # Clear the current edge from the edge drawing cache
         self._canvas.tag_lower("edge")
+        self._edges[self._edge_cache["edge"]] = EdgeBind(self._edge_cache)
         self._clear_edge_cache()
         pass
 
     def _execute_edge(self, coords):
+        """
+        Perform the operations that occur during the motion of an edge drag
+
+        :param coords:
+        :return:
+        """
         # Update the line position
         # We will update the line position by deleting and redrawing
         self._canvas.delete(self._edge_cache["edge"])
@@ -195,17 +269,32 @@ class MazePlannerCanvas(Frame):
             coords[0], coords[1], tags="edge")
 
     def _update_cache(self, item, coords):
+        """
+        Update the local cache with the item id and coordinates of the mouse
+
+        :param item:                The item with which to update the cache
+        :param coords:              The current event coordinates
+        """
         self._cache["item"] = item
         self._cache["x"] = coords[0]
         self._cache["y"] = coords[1]
 
     def _clear_cache(self, coords):
+        """
+        Clear the cache
+
+        Set the cache values to the current mouse position and None the item
+        :param coords:              The coordinates of the mouse at that event time
+        """
         self._cache["item"] = None
         self._cache["x"] = coords[0]
         self._cache["y"] = coords[1]
 
     def _clear_edge_cache(self):
-
+        """
+        Clear the edge cache to None for all values
+        :return:
+        """
         self._edge_cache["x_start"]       = None,
         self._edge_cache["y_start"]       = None,
         self._edge_cache["item_start"]    = None,
@@ -214,10 +303,20 @@ class MazePlannerCanvas(Frame):
 
 
     def _get_current_item(self, coords):
+        """
+        Return the item(if any) that the mouse is currently over
+        :param coords:                  The current coordinates of the mouse
+        :return:
+        """
         Debug.printi("X:"+ str(self._cache["x"]) + " Y:" + str(self._cache["y"]), Debug.Level.INFO)
         return self._canvas.find_overlapping(self._cache["x"], self._cache["y"], self._cache["x"], self._cache["y"])
 
     def _node_operation(self, coords):
+        """
+        Contextually create or edit a node
+        :param coords:
+        :return:
+        """
         # Determine if they are double click on the canvas, or on a node
         item = self._get_current_item(coords)
         self._cache["item"] = item
@@ -231,3 +330,13 @@ class MazePlannerCanvas(Frame):
                                 outline="red", fill="black", activeoutline="black", activefill="red")
         # then open the dialog
         NodeDialog(self, self._cache["event"].x_root+50, self._cache["event"].y_root+50)
+
+
+class EdgeBind():
+
+    def __init__(self, dict):
+        self.x_start = dict["x_start"]
+        self.y_start = dict["y_start"]
+        self.item_start = dict["item_start"]
+        self.item_end = dict["item_end"]
+        self.edge = dict["edge"]        # Use the tkinter canvas id as the specifier
