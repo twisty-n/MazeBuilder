94f437d3971996fae61583a3c3afaaa0029da44e
Tristan Newmann <tristan_dev@tristans-mbp-3.newcastle.edu.au>
2015-01-16 10:39:37 +1100

    Completed the container definitions
    Now to debug the unbound edge issue :/

--- Containers.py
+++ Containers.py
@@ -22,6 +22,7 @@ class ContainerDescriptor:
     OBJECT_CONTAINER        = "Object_Container"
     EDGE_CONTAINER          = "Edge_Container"
     WALL_CONTAINER          = "Wall_Container"
+    WALL_TEXTURE_CONTAINER  = "Wall_Texture_Container"
 
 
 
@@ -63,19 +64,19 @@ class EnvironmentContainer(Container):
 
     def fill_container(self, key_val):
 
-            # Test to see that all of the expected values have been supplied
-            if "floor_texture" not in key_val   \
-            or "wall_height" not in key_val     \
-            or "edge_width" not in key_val      \
-            or "sky_texture" not in key_val     \
-            or "start_node" not in key_val:
-                raise ContainerFillException(ContainerDescriptor.ENVIRONMENT_CONTAINER, key_val)
+        # Test to see that all of the expected values have been supplied
+        if "floor_texture" not in key_val   \
+        or "wall_height" not in key_val     \
+        or "edge_width" not in key_val      \
+        or "sky_texture" not in key_val     \
+        or "start_node" not in key_val:
+            raise ContainerFillException(ContainerDescriptor.ENVIRONMENT_CONTAINER, key_val)
 
-            self.floor_texture = key_val["floor_texture"]
-            self.wall_height = key_val["wall_height"]
-            self.edge_width = key_val["edge_width"]
-            self.sky_texture = key_val["sky_texture"]
-            self.start_node = key_val["start_node"]
+        self.floor_texture = key_val["floor_texture"]
+        self.wall_height = key_val["wall_height"]
+        self.edge_width = key_val["edge_width"]
+        self.sky_texture = key_val["sky_texture"]
+        self.start_node = key_val["start_node"]
 
     def empty_container(self):
         return  \
@@ -99,19 +100,19 @@ class VRContainer(Container):
 
     def fill_container(self, key_val):
 
-            # Test to see that all of the expected values have been supplied
-            if "frame_angle" not in key_val \
-                    or "distortion" not in key_val \
-                    or "windowed" not in key_val \
-                    or "eye_height" not in key_val \
-                    or "minimum_dist_to_wall" not in key_val:
-                raise ContainerFillException(ContainerDescriptor.VR_CONTAINER, key_val)
+        # Test to see that all of the expected values have been supplied
+        if "frame_angle" not in key_val \
+                or "distortion" not in key_val \
+                or "windowed" not in key_val \
+                or "eye_height" not in key_val \
+                or "minimum_dist_to_wall" not in key_val:
+            raise ContainerFillException(ContainerDescriptor.VR_CONTAINER, key_val)
 
-            self.frame_angle = key_val["frame_angle"]
-            self.distortion = key_val["distortion"]
-            self.windowed = key_val["windowed"]
-            self.eye_height = key_val["eye_height"]
-            self.minimum_dist_to_wall = key_val["minimum_dist_to_wall"]
+        self.frame_angle = key_val["frame_angle"]
+        self.distortion = key_val["distortion"]
+        self.windowed = key_val["windowed"]
+        self.eye_height = key_val["eye_height"]
+        self.minimum_dist_to_wall = key_val["minimum_dist_to_wall"]
 
     def empty_container(self):
         return \
@@ -134,19 +135,19 @@ class NodeContainer(Container):
 
     def fill_container(self, key_val):
 
-            # Test to see that all of the expected values have been supplied
-            if "node_id" not in key_val \
-                    or "x_coordinate" not in key_val \
-                    or "y_coordinate" not in key_val \
-                    or "room_texture" not in key_val \
-                    or "wall_pictures" not in key_val:
-                raise ContainerFillException(ContainerDescriptor.NODE_CONTAINER, key_val)
+        # Test to see that all of the expected values have been supplied
+        if "node_id" not in key_val \
+                or "x_coordinate" not in key_val \
+                or "y_coordinate" not in key_val \
+                or "room_texture" not in key_val \
+                or "wall_pictures" not in key_val:
+            raise ContainerFillException(ContainerDescriptor.NODE_CONTAINER, key_val)
 
-            self.node_id = key_val["node_id"]
-            self.x_coordinate = key_val["x_coordinate"]
-            self.y_coordinate = key_val["y_coordinate"]
-            self.room_texture = key_val["room_texture"]
-            self.wall_pictures = key_val["wall_pictures"]
+        self.node_id = key_val["node_id"]
+        self.x_coordinate = key_val["x_coordinate"]
+        self.y_coordinate = key_val["y_coordinate"]
+        self.room_texture = key_val["room_texture"]
+        self.wall_pictures = key_val["wall_pictures"]
 
     def empty_container(self):
         return \
@@ -169,19 +170,19 @@ class ObjectContainer(Container):
 
     def fill_container(self, key_val):
 
-            # Test to see that all of the expected values have been supplied
-            if "x_coordinate" not in key_val \
-                    or "y_coordinate" not in key_val \
-                    or "name" not in key_val \
-                    or "mesh" not in key_val \
-                    or "scale" not in key_val:
-                raise ContainerFillException(ContainerDescriptor.OBJECT_CONTAINER, key_val)
+        # Test to see that all of the expected values have been supplied
+        if "x_coordinate" not in key_val \
+                or "y_coordinate" not in key_val \
+                or "name" not in key_val \
+                or "mesh" not in key_val \
+                or "scale" not in key_val:
+            raise ContainerFillException(ContainerDescriptor.OBJECT_CONTAINER, key_val)
 
-            self.x_coordinate = key_val["x_coordinate"]
-            self.y_coordinate = key_val["y_coordinate"]
-            self.name = key_val["name"]
-            self.mesh = key_val["mesh"]
-            self.scale = key_val["scale"]
+        self.x_coordinate = key_val["x_coordinate"]
+        self.y_coordinate = key_val["y_coordinate"]
+        self.name = key_val["name"]
+        self.mesh = key_val["mesh"]
+        self.scale = key_val["scale"]
 
     def empty_container(self):
         return \
@@ -204,19 +205,19 @@ class EdgeContainer(Container):
 
     def fill_container(self, key_val):
 
-            # Test to see that all of the expected values have been supplied
-            if "source" not in key_val \
-                    or "target" not in key_val \
-                    or "height" not in key_val \
-                    or "wall1" not in key_val \
-                    or "wall2" not in key_val:
-                raise ContainerFillException(ContainerDescriptor.EDGE_CONTAINER, key_val)
+        # Test to see that all of the expected values have been supplied
+        if "source" not in key_val \
+                or "target" not in key_val \
+                or "height" not in key_val \
+                or "wall1" not in key_val \
+                or "wall2" not in key_val:
+            raise ContainerFillException(ContainerDescriptor.EDGE_CONTAINER, key_val)
 
-            self.source = key_val["source"]
-            self.target = key_val["target"]
-            self.height = key_val["height"]
-            self.wall1 = key_val["wall1"]
-            self.wall2 = key_val["wall2"]
+        self.source = key_val["source"]
+        self.target = key_val["target"]
+        self.height = key_val["height"]
+        self.wall1 = key_val["wall1"]
+        self.wall2 = key_val["wall2"]
 
     def empty_container(self):
         return \
@@ -253,4 +254,59 @@ class WallContainer(Container):
 
 class WallTextureContainer(Container):
     def __init__(self, key_val=None):
-        pass
+        self.path= None
+        self.tile_x = None
+        self.tile_y = None
+        self.height = None
+        Container.__init__(self, key_val)
+
+    def fill_container(self, key_val):
+
+        # Check to see that we have the required dictionary to fill the container
+        if "path" not in key_val    \
+        or "tile_x" not in key_val  \
+        or "tile_y" not in key_val  \
+        or "height" not in key_val:
+            raise ContainerFillException(ContainerDescriptor.WALL_TEXTURE_CONTAINER, key_val)
+
+        self.path = key_val["path"]
+        self.tile_x = key_val["tile_x"]
+        self.tile_y = key_val["tile_y"]
+        self.height = key_val["height"]
+
+    def empty_container(self):
+        return \
+            {
+                "path"      : self.path,
+                "tile_x"    : self.tile_x,
+                "tile_y"   : self.tile_y,
+                "height"    : self.height,
+            }
+
+class NodePictureContainer(Container):
+    def __init__(self, key_val=None):
+        self.name = None
+        self.visible = None
+        self.texture = None
+        Container.__init__(self, key_val)
+
+    def fill_container(self, key_val):
+
+        # Check to see that we have the required dictionary to fill the container
+        if "name" not in key_val \
+                or "visible" not in key_val \
+                or "texture" not in key_val:
+            raise ContainerFillException(ContainerDescriptor.WALL_TEXTURE_CONTAINER, key_val)
+
+        self.name = key_val["name"]
+        self.visible = key_val["visible"]
+        self.texture = key_val["texture"]
+
+    def empty_container(self):
+        return \
+            {
+                "name"      : self.name,
+                "visible"    : self.visible,
+                "texture"   : self.texture
+            }
+
